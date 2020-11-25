db85a9614a2c022d0bfffa0a8bab9fe0170f2647
Yulypso <khamphousone.thierry@outlook.fr>
2020-11-26 00:59:30 +0100

    test

---
+++ app/data/data_meimo_copy.js
@@ -0,0 +1,122 @@
+export default data = [
+    {
+        id: 1,
+        name: "COPY",
+        date: "22/11/2020 09:03:56",
+        overview: "Ne pas oublier de prendre son équipement de ski ce jeudi",
+        pictures: [
+            {
+              id: 1,
+              key: require('../assets/Bamboo.png')
+            }, 
+            {
+              id: 2,
+              key: require('../assets/Panda.png')
+            },
+            {
+              id: 3,
+              key : require('../assets/Bamboo.png')
+            },
+            {
+              id: 4,
+              key : require('../assets/settings.png')
+            }
+          ]
+    },
+    {
+        id: 2,
+        name: "COPY2",
+        date: "12/11/2020 17:12:03",
+        overview: "-Bamboo\n-Pavé de saumon\n-Courgette\n-Pommes\n-Petits biscuits",
+        pictures: [
+            {
+              id: 1,
+              key: require('../assets/Bamboo.png')
+            }, 
+            {
+              id: 2,
+              key: require('../assets/Panda.png')
+            },
+            {
+              id: 3,
+              key : require('../assets/Bamboo.png')
+            },
+            {
+              id: 4,
+              key : require('../assets/settings.png')
+            }
+          ]
+    },
+    {
+        id: 3,
+        name: "Recette gateau",
+        date: "08/11/2020 14:42:49",
+        overview: "-30g farine\n100kg Bamboo",
+        pictures: [
+            {
+              id: 1,
+              key: require('../assets/Bamboo.png')
+            }, 
+            {
+              id: 2,
+              key: require('../assets/Panda.png')
+            },
+            {
+              id: 3,
+              key : require('../assets/Bamboo.png')
+            },
+            {
+              id: 4,
+              key : require('../assets/settings.png')
+            }
+          ]
+    },
+    {
+        id: 4,
+        name: "Pizza Party",
+        date: "02/11/2020 19:01:37",
+        overview: "Je dois aller acheter une grosse pizza pour la fête ce samedi",
+        pictures: [
+            {
+              id: 1,
+              key: require('../assets/Bamboo.png')
+            }, 
+            {
+              id: 2,
+              key: require('../assets/Panda.png')
+            },
+            {
+              id: 3,
+              key : require('../assets/Bamboo.png')
+            },
+            {
+              id: 4,
+              key : require('../assets/settings.png')
+            }
+          ]
+    },
+    {
+        id: 5,
+        name: "First Meimo",
+        date: "15/10/2020 12:04:23",
+        overview: "Salut les amis c'est mon meimo et j'aime beaucoup manger des bamboos car je suis un panda",
+        pictures: [
+            {
+              id: 1,
+              key: require('../assets/Bamboo.png')
+            }, 
+            {
+              id: 2,
+              key: require('../assets/Panda.png')
+            },
+            {
+              id: 3,
+              key : require('../assets/Bamboo.png')
+            },
+            {
+              id: 4,
+              key : require('../assets/settings.png')
+            }
+          ]
+    },
+] 
